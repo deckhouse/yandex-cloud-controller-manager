dfad980367432bab4745b9ef3095c031bff5b00c
RomanenkoDenys <65756796+RomanenkoDenys@users.noreply.github.com>
2021-03-09 14:08:59 +0300

    move to kubernetes 1.20 code base (#32)
    * mode to kubernetes 1.20 code base
    * use initialization from cloud-provider-aws

--- cmd/yandex-cloud-controller-manager/main.go
+++ cmd/yandex-cloud-controller-manager/main.go
@@ -1,37 +1,152 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This file should be written by each cloud provider.
+// For an minimal working example, please refer to k8s.io/cloud-provider/sample/basic_main.go
+// For an advanced example, please refer to k8s.io/cloud-provider/sample/advanced_main.go
+// For more details, please refer to k8s.io/kubernetes/cmd/cloud-controller-manager/main.go
+
 package main
 
 import (
-	goflag "flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
 
-	"github.com/spf13/pflag"
-	"k8s.io/component-base/cli/flag"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/cloud-provider"
+	"k8s.io/cloud-provider/app"
+	"k8s.io/cloud-provider/options"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/cli/globalflag"
 	"k8s.io/component-base/logs"
-	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
+	"k8s.io/component-base/term"
+	"k8s.io/component-base/version/verflag"
 
 	_ "github.com/flant/yandex-cloud-controller-manager/pkg/cloudprovider/yandex"
-	_ "k8s.io/component-base/metrics/prometheus/restclient" // for client metric registration
-	_ "k8s.io/component-base/metrics/prometheus/version"    // for version metric registration
+	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
+	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
+	"k8s.io/klog/v2"
+)
+
+const (
+	yandexCloudProviderName       = "yandex"
+	yandexCloudProviderConfigFile = ""
 )
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	command := app.NewCloudControllerManagerCommand()
-	command.Use = "yandex-cloud-controller-manager"
-
-	pflag.CommandLine.SetNormalizeFunc(flag.WordSepNormalizeFunc)
-	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
-	_ = goflag.CommandLine.Parse([]string{})
+	rand.Seed(time.Now().UnixNano())
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
+	s, err := options.NewCloudControllerManagerOptions()
+	if err != nil {
+		klog.Fatalf("unable to initialize command options: %v", err)
+	}
+
+	var controllerInitializers map[string]app.InitFunc
+	command := &cobra.Command{
+		Use:  "yandex-cloud-controller-manager",
+		Long: `yandex-cloud-controller-manager manages YANDEX cloud resources for a Kubernetes cluster.`,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			// Default to the yandex provider if not set
+			cloudProviderFlag := cmd.Flags().Lookup("cloud-provider")
+			if cloudProviderFlag.Value.String() == "" {
+				_ = cloudProviderFlag.Value.Set(yandexCloudProviderName)
+			}
+
+			cloudProvider := cloudProviderFlag.Value.String()
+			if cloudProvider != yandexCloudProviderName {
+				klog.Fatalf("unknown cloud provider %s, only %s are supported", cloudProvider, yandexCloudProviderName)
+			}
+
+			cliflag.PrintFlags(cmd.Flags())
+
+			c, err := s.Config([]string{}, app.ControllersDisabledByDefault.List())
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+			// initialize cloud provider with the cloud provider name and config file provided
+			cloud, err := cloudprovider.InitCloudProvider(cloudProvider, yandexCloudProviderConfigFile)
+			if err != nil {
+				klog.Fatalf("Cloud provider could not be initialized: %v", err)
+			}
+			if cloud == nil {
+				klog.Fatalf("Cloud provider is nil")
+			}
+
+			if !cloud.HasClusterID() {
+				if c.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+					klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
+				} else {
+					klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
+				}
+			}
+
+			// Initialize the cloud provider with a reference to the clientBuilder
+			cloud.Initialize(c.ClientBuilder, make(chan struct{}))
+			// Set the informer on the user cloud object
+			if informerUserCloud, ok := cloud.(cloudprovider.InformerUser); ok {
+				informerUserCloud.SetInformers(c.SharedInformers)
+			}
+
+			controllerInitializers = app.DefaultControllerInitializers(c.Complete(), cloud)
+
+			if err := app.Run(c.Complete(), controllerInitializers, wait.NeverStop); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+		},
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
+
+	fs := command.Flags()
+	namedFlagSets := s.Flags(app.KnownControllers(controllerInitializers), app.ControllersDisabledByDefault.List())
+	verflag.AddFlags(namedFlagSets.FlagSet("global"))
+	globalflag.AddGlobalFlags(namedFlagSets.FlagSet("global"), command.Name())
+
+	for _, f := range namedFlagSets.FlagSets {
+		fs.AddFlagSet(f)
+	}
+	usageFmt := "Usage:\n  %s\n"
+	cols, _, _ := term.TerminalSize(command.OutOrStdout())
+	command.SetUsageFunc(func(cmd *cobra.Command) error {
+		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStderr(), namedFlagSets, cols)
+		return nil
+	})
+	command.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
+		cliflag.PrintSections(cmd.OutOrStdout(), namedFlagSets, cols)
+	})
+
 	if err := command.Execute(); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,74 +6,27 @@ require (
 	github.com/NYTimes/gziphandler v1.0.1 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/deckarep/golang-set v1.7.1
-	github.com/golang/protobuf v1.4.2
-	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/golang/protobuf v1.4.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.12.1 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/onsi/gomega v1.8.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/common v0.10.0
+	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
 	github.com/yandex-cloud/go-genproto v0.0.0-20200514130135-279e4db5b530
 	github.com/yandex-cloud/go-sdk v0.0.0-20200514134153-ba2dba3d5f87
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	golang.org/x/tools v0.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a
 	google.golang.org/grpc v1.28.0
-	k8s.io/api v0.19.3
-	k8s.io/apimachinery v0.19.3
-	k8s.io/client-go v0.19.3
-	k8s.io/cloud-provider v0.19.3
-	k8s.io/component-base v0.19.3
-	k8s.io/klog/v2 v2.2.0
-	k8s.io/kubernetes v1.19.3
+	k8s.io/api v0.20.4
+	k8s.io/apimachinery v0.20.4
+	k8s.io/client-go v0.20.4
+	k8s.io/cloud-provider v0.20.4
+	k8s.io/component-base v0.20.4
+	k8s.io/klog/v2 v2.4.0
 )
-
-replace k8s.io/api => k8s.io/api v0.19.3
-
-replace k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.19.3
-
-replace k8s.io/apimachinery => k8s.io/apimachinery v0.19.3
-
-replace k8s.io/apiserver => k8s.io/apiserver v0.19.3
-
-replace k8s.io/cli-runtime => k8s.io/cli-runtime v0.19.3
-
-replace k8s.io/client-go => k8s.io/client-go v0.19.3
-
-replace k8s.io/cloud-provider => k8s.io/cloud-provider v0.19.3
-
-replace k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.19.3
-
-replace k8s.io/code-generator => k8s.io/code-generator v0.19.3
-
-replace k8s.io/component-base => k8s.io/component-base v0.19.3
-
-replace k8s.io/controller-manager => k8s.io/controller-manager v0.19.3
-
-replace k8s.io/cri-api => k8s.io/cri-api v0.19.3
-
-replace k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.19.3
-
-replace k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.19.3
-
-replace k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.19.3
-
-replace k8s.io/kube-proxy => k8s.io/kube-proxy v0.19.3
-
-replace k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.19.3
-
-replace k8s.io/kubectl => k8s.io/kubectl v0.19.3
-
-replace k8s.io/kubelet => k8s.io/kubelet v0.19.3
-
-replace k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.19.3
-
-replace k8s.io/metrics => k8s.io/metrics v0.19.3
-
-replace k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.19.3
-
-replace k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.19.3
-
-replace k8s.io/sample-controller => k8s.io/sample-controller v0.19.3
