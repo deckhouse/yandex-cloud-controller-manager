edb84771b317bbc411e25378048c6dc3c17ac012
Denis Romanenko <denis.romanenko@flant.com>
2021-05-04 16:07:07 +0300

    Bump k8s version to 1.21

--- cmd/yandex-cloud-controller-manager/main.go
+++ cmd/yandex-cloud-controller-manager/main.go
@@ -27,16 +27,13 @@ import (
 	"os"
 	"time"
 
-	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/cloud-provider"
+	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
 	"k8s.io/cloud-provider/options"
 	cliflag "k8s.io/component-base/cli/flag"
-	"k8s.io/component-base/cli/globalflag"
 	"k8s.io/component-base/logs"
-	"k8s.io/component-base/term"
-	"k8s.io/component-base/version/verflag"
 
 	_ "github.com/flant/yandex-cloud-controller-manager/pkg/cloudprovider/yandex"
 	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
@@ -45,8 +42,7 @@ import (
 )
 
 const (
-	yandexCloudProviderName       = "yandex"
-	yandexCloudProviderConfigFile = ""
+	yandexCloudProviderName = "yandex"
 )
 
 func main() {
@@ -55,98 +51,46 @@ func main() {
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
-	s, err := options.NewCloudControllerManagerOptions()
+	opts, err := options.NewCloudControllerManagerOptions()
 	if err != nil {
 		klog.Fatalf("unable to initialize command options: %v", err)
 	}
 
-	var controllerInitializers map[string]app.InitFunc
-	command := &cobra.Command{
-		Use:  "yandex-cloud-controller-manager",
-		Long: `yandex-cloud-controller-manager manages YANDEX cloud resources for a Kubernetes cluster.`,
-		Run: func(cmd *cobra.Command, args []string) {
+	controllerInitializers := app.DefaultInitFuncConstructors
+	fss := cliflag.NamedFlagSets{}
 
-			// Default to the yandex provider if not set
-			cloudProviderFlag := cmd.Flags().Lookup("cloud-provider")
-			if cloudProviderFlag.Value.String() == "" {
-				_ = cloudProviderFlag.Value.Set(yandexCloudProviderName)
-			}
-
-			cloudProvider := cloudProviderFlag.Value.String()
-			if cloudProvider != yandexCloudProviderName {
-				klog.Fatalf("unknown cloud provider %s, only %s are supported", cloudProvider, yandexCloudProviderName)
-			}
-
-			cliflag.PrintFlags(cmd.Flags())
-
-			c, err := s.Config([]string{}, app.ControllersDisabledByDefault.List())
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
-				os.Exit(1)
-			}
-			// initialize cloud provider with the cloud provider name and config file provided
-			cloud, err := cloudprovider.InitCloudProvider(cloudProvider, yandexCloudProviderConfigFile)
-			if err != nil {
-				klog.Fatalf("Cloud provider could not be initialized: %v", err)
-			}
-			if cloud == nil {
-				klog.Fatalf("Cloud provider is nil")
-			}
-
-			if !cloud.HasClusterID() {
-				if c.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
-					klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
-				} else {
-					klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
-				}
-			}
-
-			// Initialize the cloud provider with a reference to the clientBuilder
-			cloud.Initialize(c.ClientBuilder, make(chan struct{}))
-			// Set the informer on the user cloud object
-			if informerUserCloud, ok := cloud.(cloudprovider.InformerUser); ok {
-				informerUserCloud.SetInformers(c.SharedInformers)
-			}
-
-			controllerInitializers = app.DefaultControllerInitializers(c.Complete(), cloud)
-
-			if err := app.Run(c.Complete(), controllerInitializers, wait.NeverStop); err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
-				os.Exit(1)
-			}
-		},
-		Args: func(cmd *cobra.Command, args []string) error {
-			for _, arg := range args {
-				if len(arg) > 0 {
-					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
-				}
-			}
-			return nil
-		},
-	}
-
-	fs := command.Flags()
-	namedFlagSets := s.Flags(app.KnownControllers(controllerInitializers), app.ControllersDisabledByDefault.List())
-	verflag.AddFlags(namedFlagSets.FlagSet("global"))
-	globalflag.AddGlobalFlags(namedFlagSets.FlagSet("global"), command.Name())
-
-	for _, f := range namedFlagSets.FlagSets {
-		fs.AddFlagSet(f)
-	}
-	usageFmt := "Usage:\n  %s\n"
-	cols, _, _ := term.TerminalSize(command.OutOrStdout())
-	command.SetUsageFunc(func(cmd *cobra.Command) error {
-		fmt.Fprintf(cmd.OutOrStderr(), usageFmt, cmd.UseLine())
-		cliflag.PrintSections(cmd.OutOrStderr(), namedFlagSets, cols)
-		return nil
-	})
-	command.SetHelpFunc(func(cmd *cobra.Command, args []string) {
-		fmt.Fprintf(cmd.OutOrStdout(), "%s\n\n"+usageFmt, cmd.Long, cmd.UseLine())
-		cliflag.PrintSections(cmd.OutOrStdout(), namedFlagSets, cols)
-	})
+	command := app.NewCloudControllerManagerCommand(opts, cloudInitializer, controllerInitializers, fss, wait.NeverStop)
 
 	if err := command.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func cloudInitializer(config *cloudcontrollerconfig.CompletedConfig) cloudprovider.Interface {
+	cloudConfig := config.ComponentConfig.KubeCloudShared.CloudProvider
+	providerName := cloudConfig.Name
+
+	if providerName == "" {
+		providerName = yandexCloudProviderName
+	}
+
+	// initialize cloud provider with the cloud provider name and config file provided
+	cloud, err := cloudprovider.InitCloudProvider(providerName, cloudConfig.CloudConfigFile)
+	if err != nil {
+		klog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+	if cloud == nil {
+		klog.Fatalf("Cloud provider is nil")
+	}
+
+	if !cloud.HasClusterID() {
+		if config.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+			klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
+		} else {
+			klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
+		}
+	}
+
+	return cloud
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/flant/yandex-cloud-controller-manager
 go 1.15
 
 require (
-	github.com/NYTimes/gziphandler v1.0.1 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/deckarep/golang-set v1.7.1
 	github.com/golang/protobuf v1.4.3
@@ -14,19 +13,15 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/common v0.10.0
 	github.com/spf13/cobra v1.1.1
-	github.com/spf13/pflag v1.0.5
 	github.com/yandex-cloud/go-genproto v0.0.0-20200514130135-279e4db5b530
 	github.com/yandex-cloud/go-sdk v0.0.0-20200514134153-ba2dba3d5f87
-	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
-	golang.org/x/tools v0.1.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a
 	google.golang.org/grpc v1.28.0
-	k8s.io/api v0.20.4
-	k8s.io/apimachinery v0.20.4
-	k8s.io/client-go v0.20.4
-	k8s.io/cloud-provider v0.20.4
-	k8s.io/component-base v0.20.4
-	k8s.io/klog/v2 v2.4.0
+	k8s.io/api v0.21.0
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
+	k8s.io/cloud-provider v0.21.0
+	k8s.io/component-base v0.21.0
+	k8s.io/klog/v2 v2.8.0
 )
